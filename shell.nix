f685b7e089c65fab5d04a11c06909dad71cd9fd1
Gurjeet Singh <gurjeet@singh.im>
2021-02-24 23:04:37 -0800

    Move shell script into the shellHook function
    This is the version that as posted to Nix community's Discourse.
    
    https://discourse.nixos.org/t/using-custom-version-of-bash-inside-nix-shell/11265/11?u=gurjeet
    
    Committing on 2021/07/20, but the code was written sometime on or before
    2021/02/24.

--- shell.nix
+++ shell.nix
@@ -3,9 +3,20 @@ with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "some_name";
   buildInputs = [
-    coreutils
     which
     bashInteractive_5
   ];
+
+  shellHook = ''
+# Strip the first element from $PATH. This component was prepended by nix-shell
+# to make sure the bash in caller's environment gets used inside nix-shell
+# environment as well.
+export PATH="$(echo "$PATH" | cut -d : -f 2-)"
+
+# Now invoke first bash found in $PATH. Use --norc to prevent invoking user's ~/.bashrc
+exec env PS1='$ ' bash --norc
+
+# Any code after the exec command above will not be executed
+  '';
 }
 
